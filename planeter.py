88865c664ed5668c4c1262eb1b9b574bf2bbfc7d
FrenzyCT <111730227+FrenzyCT@users.noreply.github.com>
2023-09-19 14:46:44 +0200

    local to remote

---
+++ planeter.py
@@ -0,0 +1,10 @@
+
+
+random_list = []
+
+def list_empty_or_not():
+    if random_list[0] == None:
+        print("list is empty")
+    else: print(f"Your list contains: " + {random_list})
+
+list_empty_or_not()
